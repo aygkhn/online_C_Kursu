c474ee499c68b7e2f0db94db5505e972f26f55b6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2019-08-26 10:58:09 +0300

    Update storage_of_types.c

--- storage_of_types.c
+++ storage_of_types.c
@@ -4,13 +4,13 @@
 
 int main()
 {
-	printf("sizeof(_Bool)		    = %zu\n", sizeof(_Bool));
-	printf("sizeof(char)		    = %zu\n", sizeof(char));
-	printf("sizeof(short)		    = %zu\n", sizeof(short));
-	printf("sizeof(int)			    = %zu\n", sizeof(int));
-	printf("sizeof(long)		    = %zu\n", sizeof(long));
-	printf("sizeof(long long)  	= %zu\n", sizeof(long long));
-	printf("sizeof(float)		    = %zu\n", sizeof(float));
-	printf("sizeof(double)		  = %zu\n", sizeof(double));
-	printf("sizeof(long double)	= %zu\n", sizeof(long double));
+	printf("sizeof(_Bool)       = %zu\n", sizeof(_Bool));
+	printf("sizeof(char)        = %zu\n", sizeof(char));
+	printf("sizeof(short)       = %zu\n", sizeof(short));
+	printf("sizeof(int)         = %zu\n", sizeof(int));
+	printf("sizeof(long)        = %zu\n", sizeof(long));
+	printf("sizeof(long long)   = %zu\n", sizeof(long long));
+	printf("sizeof(float)       = %zu\n", sizeof(float));
+	printf("sizeof(double)      = %zu\n", sizeof(double));
+	printf("sizeof(long double) = %zu\n", sizeof(long double));
 }
